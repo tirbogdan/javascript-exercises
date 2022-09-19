87be7223e47ff2cf22e7c4783cdc2e26d6ee234f
thinking <tir.bogdan1905@gmail.com>
2022-09-19 16:08:56 +0300

    Resolve the first 7 exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (temperature) {
+  return parseFloat(((temperature - 32) * (5 / 9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (temperature) {
+  return parseFloat((temperature * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
